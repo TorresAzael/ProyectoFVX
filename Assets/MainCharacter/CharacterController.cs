91da613a1d3ae6052dd2ad2df94a800e39118c64
TorresAzael <arturo_waters1998_ing@hotmail.com>
2025-10-28 12:11:30 -0600

    Control y UI pausa
    Respaldo antes del merch a la clase

--- Assets/MainCharacter/CharacterController.cs
+++ Assets/MainCharacter/CharacterController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    private Animator playerAnimator;
+    private AnimatorStateInfo animatorState;
+    private AnimatorStateInfo animatorStateWave;
+    public CapsuleCollider playerCollider;
+    public Transform objectToGrab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerAnimator.SetFloat("speed", Input.GetAxis("Vertical"));
+        playerAnimator.SetFloat("direction", Input.GetAxis("Horizontal"));
+
+        playerCollider.height = playerAnimator.GetFloat("colliderHeight");
+        Physics.gravity = Vector3.up * playerAnimator.GetFloat("gravity");
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            playerAnimator.SetTrigger("attack");
+        }
     }
+
+
 }
