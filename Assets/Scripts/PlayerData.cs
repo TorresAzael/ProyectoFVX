ccc3afaa34188677d446612c5f89789422a0547f
TorresAzael <arturo_waters1998_ing@hotmail.com>
2025-10-30 00:39:21 -0600

    UI Basico y machine library
    Prueba con biblioteca nueva y acomodo de variables

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+[Serializable]
+public class PlayerData
+{
+    public int numVidas;
+    public int numEnemigos;
+    public float valueLief;
+}
